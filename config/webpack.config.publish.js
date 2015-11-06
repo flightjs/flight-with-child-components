df8000e3914289da19f562c49a3c9cd63c529a77
Tom Ashworth <tashworth@twitter.com>
2015-11-06 14:24:41 +0000

    feat: rename global to withChildComponents

--- config/webpack.config.publish.js
+++ config/webpack.config.publish.js
@@ -3,7 +3,7 @@ var baseConfig = require('./webpack.config');
 
 module.exports = Object.assign(baseConfig, {
     output: {
-        library: 'flight-with-child-components',
+        library: 'withChildComponents',
         filename: 'flight-with-child-components.js',
         libraryTarget: 'umd',
         path: constants.BUILD_DIRECTORY
