5cfa2a77b109455414f9791a06c4fd27c364d524
Tom Ashworth <tgvashworth@gmail.com>
2017-03-27 13:17:50 +0100

    feat: add attach utility method
    * chore: don't minify published output
    * feat: add attach utility method
    * doc: document the new attach method
    * docs: further docs for attach

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,25 +2,11 @@ var webpack = require('webpack');
 
 var DedupePlugin = webpack.optimize.DedupePlugin;
 var OccurenceOrderPlugin = webpack.optimize.OccurenceOrderPlugin;
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 var plugins = [
     new DedupePlugin(),
     new OccurenceOrderPlugin()
 ];
-
-if (process.env.NODE_ENV === 'publish') {
-    plugins.push(
-        new UglifyJsPlugin({
-            compress: {
-                dead_code: true,
-                drop_console: true,
-                screw_ie8: true,
-                warnings: true
-            }
-        })
-    );
-}
 
 module.exports = {
     entry: './src',
